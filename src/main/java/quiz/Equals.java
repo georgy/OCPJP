07454a7b6dcd19e46f9a7a8b360afc676204b345
Georgy Bolyuba <bolyuba@gmail.com>
2011-12-04 21:22:44 +0100

    + maven, packages + questions

---
+++ src/main/java/quiz/Equals.java
@@ -0,0 +1,12 @@
+package quiz;
+
+public class Equals {
+
+    public static void main(String[] args) {
+        if ("String".toString() == "String")
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+
+}
